b605ecd1abade39c22421e079c8370a2c3b044bc
Faiza Husain and Nanette Girzi <nanette@>
2018-01-09 13:56:08 -0800

    Updated pet page to add Buttons to change background color.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("white-background");
+  });
+});
